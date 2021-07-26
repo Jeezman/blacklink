e6a723e4968ce6a8116e2d63eb45e0afd1b9f41b
Sarah <sarahrichie08@gmail.com>
2021-07-26 17:15:31 +0100

    LoginScreen created

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, Platform } from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +9,25 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome!</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.text_footer}>Username:</Text>
+        <View style={styles.action}>
+          <TextInput placeholder="johnny@gravy" style={styles.textInput} autoCapitalize="none" 
+          />
+        </View>
+        <View style={{marginTop: 10}}>
+          <Text style={styles.text_footer}>Password:</Text>
+          <View style={styles.action}>
+            <TextInput placeholder="**********" style={styles.textInput} autoCapitalize="none" 
+            />
+          </View>          
+        </View>
+
+      </View>
+      
     </View>
   );
 }
@@ -17,13 +35,53 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    backgroundColor: '#cecece'
   },
+  header: {
+    flex:1,
+    justifyContent: 'flex-end',
+    backgroundColor: '#cecece',
+    paddingHorizontal: 20,
+    paddingBottom: 50
+},
+  footer: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30
+  },
+  text_header: {
+    color: '#fff',
+    fontWeight: 'bold',
+   
+    fontSize: 30
+},
+  text_footer: {
+    color: '#000',
+    fontSize: 18
+},
+textInput: {
+  flex: 1,
+  height: 40,
+  borderWidth: 1,
+  paddingLeft:10,
+  
+},
   title: {
-    fontSize: 20,
+    fontSize: 30, 
+    textAlign: 'left',
     fontWeight: 'bold',
   },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    
+    paddingBottom: 5
+},
   separator: {
     marginVertical: 30,
     height: 1,
