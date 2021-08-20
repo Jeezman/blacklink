6fbb344a9967ac08226cf7d7d6cd880aacf0c5ca
Sarah <sarahrichie08@gmail.com>
2021-08-20 16:52:44 +0100

    mock-data-list added and table works

--- src/screens/Blacklist/BlacklistScreen.tsx
+++ src/screens/Blacklist/BlacklistScreen.tsx
@@ -1,39 +1,14 @@
 import * as React from 'react';
 import { View, StyleSheet, Text, ScrollView } from 'react-native';
-import { Col, Row, Grid } from 'react-native-easy-grid';
-import { BlacklistTable } from '../../components/blacklistTable';
+
+import BlacklistTables from '../../components/blacklistTables';
 
 export default function BlacklistScreen() {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-      <BlacklistTable/>
-    </View>    
-    </ScrollView>
+    <View>
+      <BlacklistTables/>    
+    </View>
+
 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-    //padding: 16,
-    //paddingTop: 100,
-    backgroundColor: '#fff',
-  },
-  cell: {
-    borderBottomWidth: 1,
-    borderColor: '#000',
-    flex: 1,
-    paddingHorizontal: 5,
-    paddingVertical: 5,
-    paddingTop:5,
-    paddingBottom:5, 
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  cellText: {
-    fontSize: 14
-  },
-});
