56a18a1caf83039a2d801123082adcda909189cc
Sarah <sarahrichie08@gmail.com>
2021-09-06 14:41:24 +0100

    setup redux

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
 import { Provider } from 'react-redux';
-import store from './src/redux/store/store';
+import {store} from './src/redux/store/store';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
